e873c7a4e1ccaaa50fbed8759c2d19a527ffa4db
aliyehiawi <ali.yehiawii@gmail.com>
2025-11-29 11:26:22 +0100

    Initial Spring Boot + H2 scaffolding

---
+++ src/main/java/com/example/waterlevel/WaterLevelMonitoringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.waterlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterLevelMonitoringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterLevelMonitoringBackendApplication.class, args);
+	}
+
+}
